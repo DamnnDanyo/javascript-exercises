7a25be285439fb71103f28ed34b1fec3bebfd0ae
Daniel Gonzalez <dgonzaleztx1990@gmail.com>
2023-02-12 15:48:46 -0600

    completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+let str = 'hey'
 
-};
+const repeatString = function(str, n){
+    if (n < 0) {
+        return 'ERROR'
+    }
+        else {
+            return str.repeat(n)
+        };
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
 
-};
 
-// Do not edit below this line
+const removeFromArray = function(arr, ...arg) {
+    
+    const newArray = arr.filter(item => !arg.includes(item));
+    
+    return newArray;
+    
+}
+
+//removeFromArray([1, 2, 3, 4], 3);
+
+// Do not edit below this line  
 module.exports = removeFromArray;
