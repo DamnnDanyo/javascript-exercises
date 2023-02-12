6ff6b935944a0faa77aa5235867d45b1b3b8d979
Daniel Gonzalez <dgonzaleztx1990@gmail.com>
2023-02-12 15:45:09 -0600

    completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,4 @@
 const sumAll = function(num1, num2) {
-    var toType = function(sortedNumber) {
-        return ({}).toString.call(sortedNumber).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
-      }
-    if (toType(num1) !== toType(num2)) {
-            return 'ERROR';
-        }
-    
-
-    const sortedNumber = [num1, num2]
-    const arrSorted = sortedNumber.sort(function(a,b){return a - b});
-     
 
     switch(true){
         case num1 < 0:
@@ -18,7 +7,17 @@ const sumAll = function(num1, num2) {
         case num2 === String:
             return 'ERROR'
     }
+
+    var toType = function(sortedNumber) {
+        return ({}).toString.call(sortedNumber).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+      }
+    if (toType(num1) !== toType(num2)) {
+            return 'ERROR';
+        }
     
+    const sortedNumber = [num1, num2]
+    const arrSorted = sortedNumber.sort(function(a,b){return a - b});
+     
         var arr = [];
             for (var i = arrSorted[0]; i <= arrSorted[1]; i++) {
             arr.push(i);
