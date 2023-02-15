8bbe30b5137751cbde237c43cbe1f55976d64ab9
Daniel Gonzalez <dgonzaleztx1990@gmail.com>
2023-02-15 15:43:25 -0600

    exercises completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let fahrenheit = (5/9) * (x - 32)
+      let fahrenheitRounded = Math.round(fahrenheit * 10) / 10
+        return fahrenheitRounded
+      
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  let celsius = 32 + (y * 9/5)
+  let celsiusRounded = Math.round(celsius * 10) / 10
+        return celsiusRounded
 };
 
 // Do not edit below this line
